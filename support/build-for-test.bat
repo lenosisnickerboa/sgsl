066414364c0a3ab846ef9edf63de577704f00fea
Lennart Oscarsson <lennart.oscarsson@gmail.com>
2026-07-11 12:36:09 +0200

    Cleaned up support files

--- support/build-for-test.bat
+++ support/build-for-test.bat
@@ -1,7 +1,8 @@
+cd ..
 if exist "dist" (
     rmdir /s /q "dist"
 )
-py -m PyInstaller --onefile --noconsole src/sgsl.py
+py -m PyInstaller --onefile src/sgsl.py
 if exist "sgsl.exe" del /q "sgsl.exe"
 copy .\dist\sgsl.exe .
 echo Done
